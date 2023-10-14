4e588b48a314eebb023225f79933978f514c8412
w9ip <m.p1nk@yandex.ru>
2023-10-14 16:13:27 +0000

    First commit

--- exmp_5_5.py
+++ exmp_5_5.py
@@ -1,3 +1,9 @@
+# Проверка перестановки.
+
+# Вариант из книги:
+# ...
+
+# Мой вариант:
 nums = tuple(map(int, input().split()))
 
 if len(nums) == max(nums) and (2 not in (nums.count(e) for e in nums)):
